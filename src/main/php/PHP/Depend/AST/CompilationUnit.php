1f480fc68fb44dae1c868b02acd5247d3cdd4252
Manuel Pichler <mapi@manuel-pichler.de>
2012-05-11 22:52:26 +0200

    Fixed: Now CompilationUnit is also a PHP_Depend AST node.

--- src/main/php/PHP/Depend/AST/CompilationUnit.php
+++ src/main/php/PHP/Depend/AST/CompilationUnit.php
@@ -1,16 +1,92 @@
 <?php
 /**
+ * This file is part of PHP_Depend.
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2008-2012, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage AST
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2012 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://pdepend.org/
+ */
+
+/**
+ * Custom AST node that represents a compilation unit.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage AST
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2012 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://pdepend.org/
+ * @since      2.0.0
+ *
+ * @property string $file
  * @property PHPParser_Node_Stmt[] $stmts
  */
-class PHP_Depend_AST_CompilationUnit extends PHPParser_NodeAbstract
+class PHP_Depend_AST_CompilationUnit extends PHPParser_NodeAbstract implements PHP_Depend_AST_Node
 {
     /**
      * Constructs a new compilation unit.
      *
+     * @param string $file
      * @param PHPParser_Node_Stmt[] $stmts
      */
-    public function __construct( array $stmts )
+    public function __construct( $file, array $stmts )
     {
-        parent::__construct( array( 'stmts' => $stmts ) );
+        parent::__construct(
+            array(
+                'file'  => $file,
+                'stmts' => $stmts
+            )
+        );
+    }
+
+    /**
+     * Returns the global identifier for this node.
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return md5( $this->file );
     }
 }
