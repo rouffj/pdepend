47ef1ef23b3aa1e05958a5506aaaf4f81cb8ef96
Manuel Pichler <mapi@manuel-pichler.de>
2012-05-19 21:19:09 +0200

    Fixed: Missing doc comments added.

--- src/main/php/PHP/Depend/AST/Namespace.php
+++ src/main/php/PHP/Depend/AST/Namespace.php
@@ -1,4 +1,64 @@
 <?php
+/**
+ * This file is part of PHP_Depend.
+ *
+ * PHP Version 5
+ *
+ * Copyright (c) 2008-2012, Manuel Pichler <mapi@pdepend.org>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Manuel Pichler nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage AST
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2012 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://pdepend.org/
+ */
+
+/**
+ * Custom AST node that represents a PHP namespace/package.
+ *
+ * @category   QualityAssurance
+ * @package    PHP_Depend
+ * @subpackage AST
+ * @author     Manuel Pichler <mapi@pdepend.org>
+ * @copyright  2008-2012 Manuel Pichler. All rights reserved.
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://pdepend.org/
+ * @since      2.0.0
+ */
 class PHP_Depend_AST_Namespace
     extends PHPParser_Node_Stmt_Namespace
     implements PHP_Depend_AST_Node
@@ -9,6 +69,12 @@ class PHP_Depend_AST_Namespace
      */
     private $refs;
 
+    /**
+     * Constructs a new namespace instance.
+     *
+     * @param PHPParser_Node_Stmt_Namespace $namespace
+     * @param PHP_Depend_AST_NamespaceRefs $refs
+     */
     public function __construct( PHPParser_Node_Stmt_Namespace $namespace, PHP_Depend_AST_NamespaceRefs $refs )
     {
         parent::__construct(
@@ -22,11 +88,23 @@ class PHP_Depend_AST_Namespace
         $this->refs->initialize( $this );
     }
 
+    /**
+     * Returns the global identifier for this node.
+     *
+     * @return string
+     */
     public function getId()
     {
         return $this->getAttribute( 'id' );
     }
 
+    /**
+     * Magic wake up method that will register this object in the global node
+     * reference context.
+     *
+     * @return void
+     * @access private
+     */
     public function __wakeup()
     {
         $this->refs->initialize( $this );
